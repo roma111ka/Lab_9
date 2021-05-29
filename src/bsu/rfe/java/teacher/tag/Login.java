de3ca1199f0c3a3ab7d8ae9e1bb7718305488c2d
Roman_Lodyaty <romalodyaty@gmail.com>
2021-05-29 17:13:21 +0300

    add: tag classes

---
+++ src/bsu/rfe/java/teacher/tag/Login.java
@@ -0,0 +1,40 @@
+package bsu.rfe.java.teacher.tag;
+
+import java.io.IOException;
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.PageContext;
+import javax.servlet.jsp.tagext.SimpleTagSupport;
+import bsu.rfe.java.teacher.entity.User;
+import bsu.rfe.java.teacher.entity.UserList;
+
+public class Login extends SimpleTagSupport {
+    private String login;
+    private String password;
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void doTag() throws JspException, IOException {
+        String errorMessage = null;
+        UserList userList = (UserList)
+                getJspContext().getAttribute("users", PageContext.APPLICATION_SCOPE);
+        if (login==null || login.equals("")) {
+            errorMessage = "Логин не может быть пустым!";
+        } else {
+            User user = userList.findUser(login);
+            if (user==null || !user.getPassword().equals(password)) {
+                errorMessage = "Такой пользователь не существует или\n"
+                        + "указанная комбинация логин/пароль неверна!";
+            } else {
+                getJspContext().setAttribute("authUser", user,
+                        PageContext.SESSION_SCOPE);
+            }
+        }
+        getJspContext().setAttribute("errorMessage", errorMessage,
+                PageContext.SESSION_SCOPE);
+    }
+}
